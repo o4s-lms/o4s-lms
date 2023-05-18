ba82624392a54c5754371e6921c6d2c3bd3d6758
José Cordeiro <joseantcordeiro@gmail.com>
2023-05-18 03:55:35 +0000

    Api routes

---
+++ apps/api/tmp/lessons/create.ts
@@ -0,0 +1,26 @@
+import slugify from '@sindresorhus/slugify';
+import { createOperation, z } from '../../generated/wundergraph.factory';
+
+export default createOperation.mutation({
+  // by specifying the input schema, we're automatically creating a JSON Schema for input validation
+  input: z.object({
+		name: z.string(),
+    courseId: z.number(),
+    moduleId: z.number(),
+  }),
+  handler: async ({ user, input, graph }) => {
+		const userId = String(user?.userId);
+		const lesson = await graph
+			.from('lms')
+			.mutate('createOneLesson')
+			.where({
+				name: input.name,
+				slug: slugify(input.name),
+				courseId: input.courseId,
+				moduleId: input.moduleId,
+				createdBy: userId,
+			})
+			.exec();
+		return lesson;
+  },
+});
