8184f9cb4bd278b5c36036b6195895a9eeb70fe5
José Cordeiro <joseantcordeiro@gmail.com>
2023-05-27 05:47:21 +0000

    lms web front

--- apps/web/app/x/page.tsx
+++ apps/web/app/x/page.tsx
@@ -3,7 +3,6 @@
 import Link from "next/link"
 import { useSession } from "next-auth/react"
 import { useQuery } from "@/lib/wundergraph"
-import { type UsersMy_coursesResponseData } from "@o4s/generated-wundergraph/models"
 
 import { siteConfig } from "@/config/site"
 import { Button, buttonVariants } from "@/components/ui/button"
@@ -12,8 +11,7 @@ import { Loading } from "@/components/loading"
 import { useToast } from "@/hooks/use-toast"
 import { CourseCard } from "./components/course-card"
 import { Item } from "@radix-ui/react-dropdown-menu"
-
-type Courses = UsersMy_coursesResponseData["users"]
+import { Separator } from "@/components/ui/separator"
 
 export default function Dashboard() {
 	const { toast } = useToast()
@@ -21,7 +19,7 @@ export default function Dashboard() {
 	const { data, error, isLoading } = useQuery({
 		operationName: 'members/courses',
 		input: {
-			role: 'STUDENT',
+			role: 'ADMIN',
 		},
 		enabled: true,
 	})
@@ -40,18 +38,25 @@ export default function Dashboard() {
 	
   return (
 		<>
-		<div className="container grid items-center space-y-6">
-			<div className="gap-6 pb-8 pt-6 md:py-10">
-				{!isLoading ? (
-					<>
-					{data?.courses.map(item => (
-						<CourseCard item={item} />
-					))}
-					</>
-				) : (
-					<Loading />
-				)}
-      </div>
+		<div className="container grid items-center space-y-6 p-10 pb-16 md:block">
+        <div className="space-y-0.5">
+          <h2 className="text-2xl font-bold tracking-tight">Courses</h2>
+          <p className="text-muted-foreground">
+            Manage your courses.
+          </p>
+        </div>
+        <Separator className="my-6" />
+				<div className="pb-8 pt-6 md:py-10">
+					{!isLoading ? (
+						<>
+						{data?.courses.map(item => (
+							<CourseCard item={item} />
+						))}
+						</>
+					) : (
+						<Loading />
+					)}
+				</div>
 		</div>
 		</>
   )
