cfb60a6bc0a52f574a7b9a9ff4ab000083d64bfa
José Cordeiro <joseantcordeiro@gmail.com>
2023-06-22 22:40:31 +0000

    lms courses

---
+++ apps/api/operations/tasks/count-status.ts
@@ -0,0 +1,39 @@
+import { createOperation, z } from '../../generated/wundergraph.factory'
+import { OperationError } from '@wundergraph/sdk/operations'
+
+export class LessonProgressError extends OperationError {
+  statusCode = 400;
+  code = 'LessonProgressError' as const;
+  message = 'Lesson progress query error';
+}
+
+export class LessonsError extends OperationError {
+  statusCode = 400;
+  code = 'LessonsError' as const;
+  message = 'Lessons query error';
+}
+
+export default createOperation.query({
+	input: z.object({
+		course_id: z.string(),
+  }),
+	handler: async ({ input, user, graph }) => {
+		const status = await graph
+			.from('lms')
+			.query('groupByTask')
+			.where({
+				by: ['status'],
+				where: {
+					course_id: input.course_id,
+					created_by: user?.userId as string,
+				},
+				select: {
+					_count: {
+						status: true,
+					},
+				},
+			})
+			.exec()
+		return status
+	},
+})
