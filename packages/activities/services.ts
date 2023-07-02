e8bf3a03837df7b81c67487440da1bb1722c841b
José Cordeiro <joseantcordeiro@gmail.com>
2023-07-02 01:16:19 +0000

    temporal activities

--- packages/activities/services.ts
+++ packages/activities/services.ts
@@ -1,4 +1,5 @@
 import { ApplicationFailure, Context } from '@temporalio/activity'
+import { createTransport, type SendMailOptions } from 'nodemailer'
 
 export const notificationService = {
   sendNotification({ type, message }: { type: string; message: string }) {
@@ -8,6 +9,21 @@ export const notificationService = {
 
     console.log(`Sent ${type} notification: ${message}`)
   },
+	sendEmail(props: Pick<SendMailOptions, 'to' | 'html' | 'subject'>) {
+		const transporter = createTransport({
+			host: process.env.EMAIL_SERVER_HOST,
+			port: Number(process.env.EMAIL_SERVER_PORT),
+			auth: {
+				user: process.env.EMAIL_SERVER_USER,
+				pass: process.env.EMAIL_SERVER_PASSWORD,
+			},
+		})
+	
+		return transporter.sendMail({
+			from: process.env.EMAIL_FROM ?? 'noreply@o4s-lms.com',
+			...props,
+		})
+	},
 }
 
 export const paymentService = {
