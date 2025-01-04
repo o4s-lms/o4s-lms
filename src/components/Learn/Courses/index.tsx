a3b7bbc178ca3374d36cb9739bdf0d06ca97ecbc
José Cordeiro <joseantcordeiro@gmail.com>
2025-01-04 10:09:23 +0000

    bug key in courses page

--- src/components/Learn/Courses/index.tsx
+++ src/components/Learn/Courses/index.tsx
@@ -64,7 +64,7 @@ export function CoursesContent({ userId, enrollments }: CoursesProps) {
 
   return (
     <Main fixed>
-      <div id="courses-content" className="flex flex-1 flex-col gap-4 p-4">
+      <div key="courses-content" className="flex flex-1 flex-col gap-4 p-4">
         <span className="relative flex justify-center">
           <div className="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75"></div>
 
