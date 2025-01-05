4189bb589ccf0d4443155f14955821fc7dab1ed0
José Cordeiro <joseantcordeiro@gmail.com>
2025-01-05 16:28:57 +0000

    support page

---
+++ src/app/(frontend)/(authorized)/dashboard/support/page.tsx
@@ -0,0 +1,58 @@
+import type { Metadata } from 'next';
+import ComingSoon from '@/components/ComingSoon';
+import { LanguageSelector } from '@/components/LangSelector.';
+import { Header } from '@/components/Layout/Header';
+import { TopNav } from '@/components/Layout/TopNav';
+import { ProfileDropdown } from '@/components/ProfileDropdown';
+import { ThemeSwitch } from '@/components/ThemeSwitch';
+
+export const metadata: Metadata = {
+  //metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL),
+  title: 'Support | O4S LMS',
+  description: 'Get started with your courses.',
+};
+
+export default function SupportPage() {
+  return (
+    <>
+      <Header>
+        <TopNav links={topNav} />
+        <div className="ml-auto flex items-center space-x-4">
+          {/**<Search />
+            <ThemeSwitch />*/}
+          <LanguageSelector />
+          <ThemeSwitch />
+          <ProfileDropdown />
+        </div>
+      </Header>
+      <ComingSoon />
+    </>
+  );
+}
+
+const topNav = [
+  {
+    title: 'Overview',
+    href: 'dashboard/overview',
+    isActive: true,
+    disabled: false,
+  },
+  {
+    title: 'Customers',
+    href: 'dashboard/customers',
+    isActive: false,
+    disabled: true,
+  },
+  {
+    title: 'Products',
+    href: 'dashboard/products',
+    isActive: false,
+    disabled: true,
+  },
+  {
+    title: 'Settings',
+    href: 'dashboard/settings',
+    isActive: false,
+    disabled: true,
+  },
+];
