43ab25e71791a7897313bdfcd0feaa60d0c83419
José Cordeiro <joseantcordeiro@gmail.com>
2025-01-12 07:45:38 +0000

    ask ai

--- src/components/Learn/AskAi/index.tsx
+++ src/components/Learn/AskAi/index.tsx
@@ -4,13 +4,12 @@ import { Main } from '@/components/Layout/Main';
 import { FullPageChat } from 'flowise-embed-react';
 
 export function AskAi() {
-
   return (
     <Main fixed>
       <div key="ask-ai-content" className="flex flex-1 flex-col gap-4 p-4">
         <FullPageChat
-          chatflowid="2baa386f-c30e-4d59-9697-f95e77014da7"
-          apiHost="http://localhost:3001"
+          chatflowid="0fc98301-71b7-4e58-966f-c395d0a10ffe"
+          apiHost="http://joseantcordeiro.hopto.org:3001"
         />
       </div>
     </Main>
