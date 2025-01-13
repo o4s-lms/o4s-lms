4610a9646a9774495eae4d9219aa1fc116807057
José Cordeiro <joseantcordeiro@gmail.com>
2025-01-13 08:31:13 +0000

    compute course progress

--- src/components/Learn/Course/index.tsx
+++ src/components/Learn/Course/index.tsx
@@ -9,7 +9,15 @@ import { getLessonById, getLessonProgress } from '@/utilities/lessons';
 import { usePathname } from 'next/navigation';
 import type { LessonProgress } from '@/payload-types';
 import { Speeddial } from '@/components/ui/animata/fabs/speed-dial';
-import { Check, Share2, SquarePen, Star, StarOff, Trash, X } from 'lucide-react';
+import {
+  Check,
+  Share2,
+  SquarePen,
+  Star,
+  StarOff,
+  Trash,
+  X,
+} from 'lucide-react';
 import { Main } from '@/components/Layout/Main';
 import { ContentSection } from './ContentSection';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
@@ -119,30 +127,35 @@ export function CourseContent({ userId, courseId }: CourseProps) {
 
   const handleFavorite = () => {
     if (isFavorite) {
-      removeFavorite.mutate()
+      removeFavorite.mutate();
     } else {
-      createFavorite.mutate()
+      createFavorite.mutate();
     }
-  }
+  };
 
   const renderIcon = () => {
-    if (isFavorite) return <StarOff />
-    return <Star />
-  }
+    if (isFavorite) return <StarOff />;
+    return <Star />;
+  };
 
   const renderCompletedIcon = () => {
-    if (progress?.completed) return <X />
-    return <Check />
-  }
+    if (progress?.completed) return <X />;
+    return <Check />;
+  };
 
   return (
     <Main fixed>
-      <div key="lesson-content" className="flex flex-1 flex-col space-y-2 overflow-hidden md:space-y-2 lg:flex-row lg:space-x-12 lg:space-y-0">
-        <aside className="hidden lg:block top-0 lg:sticky lg:w-16">
+      <div
+        key="lesson-content"
+        className="flex flex-1 flex-col space-y-2 overflow-hidden md:space-y-2 lg:flex-row lg:space-x-12 lg:space-y-0"
+      >
+        <aside className="top-0 hidden lg:sticky lg:block lg:w-16">
           <Speeddial
             actionButtons={[
               {
-                action: () => {handleFavorite()},
+                action: () => {
+                  handleFavorite();
+                },
                 //icon: {!isFavorite ? <Star /> : <StarOff />},
                 icon: renderIcon(),
                 key: 'favorite',
@@ -150,14 +163,18 @@ export function CourseContent({ userId, courseId }: CourseProps) {
               },
               {
                 action: async () => {
-                  const l = await lessonCompleted.mutateAsync(!progress?.completed);
-                  if (l) setLessonProgress(l);
-                  fetcher(
-                    `/api/functions/computeCourseProgress?userId=${userId}&courseId=${courseId}`,
-                    {
-                      method: 'POST',
-                    },
+                  const l = await lessonCompleted.mutateAsync(
+                    !progress?.completed,
                   );
+                  if (l) {
+                    setLessonProgress(l);
+                    fetcher(
+                      `/api/functions/computeCourseProgress?userId=${userId}&courseId=${courseId}`,
+                      {
+                        method: 'POST',
+                      },
+                    );
+                  }
                 },
                 icon: renderCompletedIcon(),
                 key: 'complete',
@@ -180,7 +197,7 @@ export function CourseContent({ userId, courseId }: CourseProps) {
           />
         </aside>
 
-        <aside className="lg:hidden top-0 lg:sticky lg:h-16">
+        <aside className="top-0 lg:sticky lg:hidden lg:h-16">
           <Speeddial
             actionButtons={[
               {
@@ -213,7 +230,11 @@ export function CourseContent({ userId, courseId }: CourseProps) {
         </aside>
 
         <div className="flex w-full overflow-y-hidden p-1 pr-4">
-          <ContentSection title={lesson?.title} desc={`Last access: ${format(progress?.lastAccessed as string)} - Progress: ${progress?.completed ? `Completed ${format(progress?.completedAt as string)}` : 'In Progress'}`} completed={progress?.completed}>
+          <ContentSection
+            title={lesson?.title}
+            desc={`Last access: ${format(progress?.lastAccessed as string)} - Progress: ${progress?.completed ? `Completed ${format(progress?.completedAt as string)}` : 'In Progress'}`}
+            completed={progress?.completed}
+          >
             <RichText
               className="mx-auto max-w-[96rem]"
               data={lesson?.content}
