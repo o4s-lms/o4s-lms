14fc1005c0d74d34b16ce0d092a393a6e2d76a31
José Cordeiro <joseantcordeiro@gmail.com>
2025-01-14 11:27:42 +0000

    upgrade payload and graphql

--- src/app/(frontend)/(authorized)/learn/page.tsx
+++ src/app/(frontend)/(authorized)/learn/page.tsx
@@ -47,8 +47,7 @@ export default async function Page() {
         <Header>
           <TopNav links={topNav} />
           <div className="ml-auto flex items-center space-x-4">
-            {/**<Search />
-                             <ThemeSwitch />*/}
+            {/**<Search />*/}
             <LanguageSelector />
             <ThemeSwitch />
             <ProfileDropdown />
