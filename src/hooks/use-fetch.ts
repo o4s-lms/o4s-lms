ffec232a969389aa994612a28fcfe3537bc83b41
José Cordeiro <joseantcordeiro@gmail.com>
2025-01-30 19:58:31 +0000

    useFetch

---
+++ src/hooks/use-fetch.ts
@@ -0,0 +1,27 @@
+import { fetcher } from "@/lib/fetcher";
+import { useState, useEffect } from "react";
+
+export function useFetch<T>(url: string) {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetcher(url);
+        const result = await response.json();
+        setData(result);
+      } catch (err) {
+        setError(err as Error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+}
